02ba8cd4f5db725f0a21c84f01ad8f7df35710a8
Darren Waddell <darren@forwardhealth.co>
2019-02-15 19:09:18 +0000

    Fixing layout scroll issues

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,16 +1,15 @@
 /* eslint-disable */
 
-
 /* SPLASH */
 $(function(){
-  $('[data-behaviour="go-to-choose-city"]').on('touchstart', function(evt){
+  $('[data-behaviour="go-to-choose-city"]').on('click touchstart', function(evt){
     evt.preventDefault();
   
     $('.splash').css('display', 'none');
     $('.cities').css('display', 'block');
   });
 
-  $('[data-behaviour="go-to-information"]').on('touchstart', function(evt){
+  $('[data-behaviour="go-to-information"]').on('click touchstart', function(evt){
     evt.preventDefault();
   
     $('.splash').css('display', 'none');
@@ -23,7 +22,7 @@ $(function(){
 $(function(){
   $('.cities .list-item[data-city="Barcelona"]').css('display', 'none');
 
-  $('.cities .list button').on('touchstart', function(evt){
+  $('.cities .list button').on('click touchstart', function(evt){
     evt.preventDefault();
 
     const selected = $(this).text();
@@ -41,7 +40,7 @@ $(function(){
 
 /* INFORMATION */
 $(function(){
-  $('[data-behaviour="go-to-sign-up"]').on('touchstart', function(evt){
+  $('[data-behaviour="go-to-sign-up"]').on('click touchstart', function(evt){
     evt.preventDefault();
 
     $('.information').css('display', 'none');
