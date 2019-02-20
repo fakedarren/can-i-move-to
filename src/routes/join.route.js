68053bfb5e56daba2da1b4b2228bd8f0b8622d12
Darren Waddell <darren@forwardhealth.co>
2019-02-20 15:00:29 +0000

    YOLO

--- src/routes/join.route.js
+++ src/routes/join.route.js
@@ -16,7 +16,7 @@ module.exports = async (req, res, next) => {
     lastName = '';
   }
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
 
   await page.goto('https://www.bestforbritain.org/join', { waitUntil: 'load' });
