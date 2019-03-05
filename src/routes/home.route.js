ea4c91d7ceeeddb4a34b72694f921556c261b6b9
James Taylor <jt-git@nti.me.uk>
2019-03-05 22:00:35 +0000

    Add city routes with history support
    Signed-off-by: James Taylor <jt-git@nti.me.uk>

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -3,7 +3,11 @@ const stories = require('../data/stories');
 
 
 module.exports = (req, res, next) => {
+  const page = req.path.substring(1);
+  const city = page ? page.charAt(0).toUpperCase() + page.slice(1) : 'Barcelona';
+
   res.render('pages/home', {
+    city,
     cities,
     stories,
   });
--- src/service/register-routes.js
+++ src/service/register-routes.js
@@ -1,11 +1,15 @@
 const home = require('../routes/home.route');
 const join = require('../routes/join.route');
-
+const cities = require('../data/cities.json');
 
 module.exports = {
 
   register: (app) => {
     app.get('/', home);
+    cities.forEach(city => {
+      const path = city.toLowerCase();
+      app.get(`/${path}`, home);
+    });
     app.post('/join', join);
   },
 
