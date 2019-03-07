c1f7079520edc4d8a2731ab7bc8b26800a27272a
Darren Waddell <darren.waddell@gmail.com>
2019-03-07 11:18:06 +0000

    Fix for pictures

--- src/lib/handlebars-helpers.js
+++ src/lib/handlebars-helpers.js
@@ -2,5 +2,7 @@ module.exports = {
 
   log: obj => JSON.stringify(obj, null, '\t'),
 
+  lower: (text) => text.toLowerCase(),
+
   'tweet-content': () => encodeURIComponent("Can I move to Barcelona? What about Athens? Prague? Copenhagen? Vienna? Paris? If Brexit happens, we UK citizens will lose our #freedomofmovement - the right to move anywhere in the EU, no matter our age, income or working status. Find out more on https://canImovetobarcelona.com #LoveEU")
 };
